6573a072ec5c1263ca3432839a9c787de0024a73
davidlatwe <david962041@gmail.com>
2018-10-13 18:51:36 +0800

    Merge .utils to .api

--- mdiff/api.py
+++ mdiff/api.py
@@ -4,6 +4,14 @@ from .core import (
     update_identity,
     lock_identity,
 
+    is_duplicated,
+    is_changed,
+    get_time,
+    read_uuid,
+    read_address,
+    read_verifier,
+    read_fingerprint,
+
     ATTR_ADDRESS,
     ATTR_VERIFIER,
     ATTR_FINGERPRINT,
@@ -14,6 +22,14 @@ __all__ = (
     "is_update_required",
     "update_identity",
     "lock_identity",
+
+    "is_duplicated",
+    "is_changed",
+    "get_time",
+    "read_uuid",
+    "read_address",
+    "read_verifier",
+    "read_fingerprint",
 
     "ATTR_ADDRESS",
     "ATTR_VERIFIER",
